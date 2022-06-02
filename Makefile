2d9fd13c056614d5a37b1e7db8c3ada04081940f
FunnyDrew <rybin.ag@gmail.com>
2022-06-02 16:39:06 +0300

    add eslinter

--- Makefile
+++ Makefile
@@ -5,6 +5,9 @@ lint:
 	npx htmlhint *.html ./src/pages/*.html
 	npx stylelint ./src/styles/*.css ./src/styles/pages_styles/*.css
 	
+lintjs:
+	npx eslint src/scripts/*.js
+	
 start:
 	npx browser-sync start -s  --files 'src/pages/*.html'
 
--- src/scripts/rock_paper_scissors.js
+++ src/scripts/rock_paper_scissors.js
@@ -1,57 +1,72 @@
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('button.game');
+console.log(buttons);
 
 const playerScoresDiv = document.querySelector('.player-scores');
 const computerScoresDiv = document.querySelector('.computer-scores');
 
 const playerChooseDiv = document.querySelector('.player-choose');
 const computerChooseDiv = document.querySelector('.computer-choose');
+const btnStart = document.querySelector('.start');
+
+const layout = document.querySelector('.layout-black');
 
 let playerScores = 0;
 let computerScores = 0;
 
 const initGame = () => {
+  playerScores = 0;
+  computerScores = 0;
 
-    playerScores = 0;
-    computerScores = 0;
+  playerScoresDiv.textContent = 0;
+  computerScoresDiv.textContent = 0;
 
-    playerScoresDiv.textContent = 0;
-    computerScoresDiv.textContent = 0;
+  playerChooseDiv.textContent = '';
+  computerChooseDiv.textContent = '';
+  layout.classList.toggle('active', false);
+  btnStart.classList.toggle('active', false);
+};
 
-    playerChooseDiv.textContent = '';
-    computerChooseDiv.textContent = '';
-}
-
-const playMap = [[0, "c", "p"], ["p", 0, "c"], ["c", "p", 0]];
+const playMap = [
+  [0, 'c', 'p'],
+  ['p', 0, 'c'],
+  ['c', 'p', 0],
+];
 const variants = ['rock', 'paper', 'scissors'];
-const mapToScore = {0: [0, 0],
-                   "p": [1, 0],
-                   "c": [0, 1]};
+const mapToScore = { 0: [0, 0], p: [1, 0], c: [0, 1] };
 
-const makeStep = () => Math.floor(Math.random()*3);
+const makeStep = () => Math.floor(Math.random() * 3);
+const emojiMap = { rock: '👊', paper: '🤚', scissors: '✌️' };
 
 initGame();
 
-buttons.forEach((button) => button.addEventListener('click', (e) => {
+btnStart.addEventListener('click', (e) => {
+  e.preventDefault();
+  initGame();
+});
+
+buttons.forEach((button) =>
+  button.addEventListener('click', (e) => {
     e.preventDefault();
-    
-    const playerChoose = variants.indexOf(e.target.id);    
+
+    const playerChoose = variants.indexOf(e.target.id);
     const computerChoose = makeStep();
 
-    playerChooseDiv.textContent = e.target.id;
-    computerChooseDiv.textContent = variants[computerChoose];
-    
-    const [playerScore, computerScore] = mapToScore[playMap[playerChoose][computerChoose]];
+    playerChooseDiv.textContent = emojiMap[e.target.id];
+    computerChooseDiv.textContent = emojiMap[variants[computerChoose]];
+
+    const [playerScore, computerScore] =
+      mapToScore[playMap[playerChoose][computerChoose]];
     playerScores += playerScore;
     computerScores += computerScore;
     playerScoresDiv.textContent = playerScores;
     computerScoresDiv.textContent = computerScores;
-    
-    if ((playerScores === 5) || (computerScores === 5)){
-        playerScoresDiv.textContent = playerScores;
-    computerScoresDiv.textContent = computerScores;
-        alert("game end");
-        initGame();
+
+    if (playerScores === 5 || computerScores === 5) {
+      playerScoresDiv.textContent = playerScores;
+      computerScoresDiv.textContent = computerScores;
+      layout.classList.add('active');
+
+      btnStart.classList.add('active');
     }
-                
-    })
+  })
 );
